c81a3b741f8887292d07a9ac4eb05187b7f92429
Sercan Özen <srcn.ozn5@gmail.com>
2023-03-25 17:16:13 +0300

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        URL::forceScheme('https');
     }
 
     /**
